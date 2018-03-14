d31da9c32fb06fcdf953c9adca9e8673025edff2
zc.xue <xuezhichao@yun-jinrong.com>
2018-03-14 17:49:05 +0800

    redis缓存
    添加redis缓存

--- src/main/java/com/yunjinrong/springbootmybatis/controller/UserController.java
+++ src/main/java/com/yunjinrong/springbootmybatis/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.github.pagehelper.Page;
 import com.yunjinrong.springbootmybatis.model.User;
 import com.yunjinrong.springbootmybatis.service.UserService;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -38,6 +39,24 @@ public class UserController {
     public Page<User> findUser(@RequestBody User user) {
     	Page<User> p = userService.findUserList(user.getPage(),user.getRows());
     	return p;
+    }
+    
+    @ApiOperation(value = "用户新增",notes="用户新增")
+    @ApiImplicitParams({
+    @ApiImplicitParam(paramType="body",dataType="User",name="user",value="用户",required=true)})
+    @RequestMapping(value="insertUser",method=RequestMethod.POST)
+    public Boolean insertUser(@RequestBody User user) {
+    	userService.insertUser(user);
+    	return true;
+    }
+    
+    @ApiOperation(value = "用户新增",notes="用户新增")
+    @ApiImplicitParams({
+    @ApiImplicitParam(paramType="body",dataType="Long",name="id",value="用户id",required=true)})
+    @RequestMapping(value="delUser",method=RequestMethod.DELETE)
+    public Boolean delUser(@RequestParam Long id) {
+    	userService.deleteUser(id);
+    	return true;
     }
     
 }
--- src/main/java/com/yunjinrong/springbootmybatis/service/impl/UserServiceImpl.java
+++ src/main/java/com/yunjinrong/springbootmybatis/service/impl/UserServiceImpl.java
@@ -1,6 +1,9 @@
 package com.yunjinrong.springbootmybatis.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.github.pagehelper.Page;
@@ -21,25 +24,30 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
+	//@CachePut缓存新增的或更新的数据到缓存,其中缓存名字是 people 。数据的key是person的id
+    @CachePut(value = "user", key = "#p0.id")
 	public int insertUser(User user) {
 		return userMapper.insert(user);
 	}
 
 	@Override
+	//@CacheEvict 从缓存people中删除key为id 的数据
+    @CacheEvict(value = "user")
 	public int deleteUser(Long id) {
-		// TODO Auto-generated method stub
 		return userMapper.deleteByPrimaryKey(id);
 	}
 
 	@Override
+	//@CachePut缓存新增的或更新的数据到缓存,其中缓存名字是 people 。数据的key是person的id
+    @CachePut(value = "user", key = "#user.id")
 	public int updateUserById(User user) {
-		// TODO Auto-generated method stub
 		return userMapper.updateByPrimaryKey(user);
 	}
 
 	@Override
+	//@Cacheable缓存key为person 的id 数据到缓存people 中,如果没有指定key则方法参数作为key保存到缓存中。
+    @Cacheable(value = "user", key = "#id")
 	public User fingUserById(Long id) {
-		// TODO Auto-generated method stub
 		return userMapper.selectByPrimaryKey(id);
 	}
 
