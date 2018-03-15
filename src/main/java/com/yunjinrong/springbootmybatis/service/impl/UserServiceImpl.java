be6c3b763a1de7326888df06a56fab25c6dcaaf0
zc.xue <xuezhichao@yun-jinrong.com>
2018-03-15 17:34:51 +0800

    @Transactional
    事务控制

--- src/main/java/com/yunjinrong/springbootmybatis/service/impl/UserServiceImpl.java
+++ src/main/java/com/yunjinrong/springbootmybatis/service/impl/UserServiceImpl.java
@@ -6,6 +6,9 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
@@ -21,13 +24,18 @@ public class UserServiceImpl implements UserService {
     private UserMapper userMapper;
 	
 	@Override
+	@Transactional(readOnly=true)
 	public String selectAgeByName(String name) {
 		return userMapper.selectAgeByName(name);
 	}
 
-	@Override
 	//@CachePut缓存新增的或更新的数据到缓存,其中缓存名字是 people 。数据的key是person的id
     @CachePut(key = "#p0.id")
+	/**
+	 * ISOLATION 事务的隔离级别
+	 * PROPAGATION 事务的传播行为
+	 */
+	@Transactional
 	public User insertUser(User user) {
 		userMapper.insert(user);
 		return user;
@@ -36,6 +44,7 @@ public class UserServiceImpl implements UserService {
 	@Override
 	//@CacheEvict 从缓存people中删除key为id 的数据
     @CacheEvict(key = "#p0")
+	@Transactional
 	public int deleteUser(String id) {
 		return userMapper.deleteByPrimaryKey(id);
 	}
@@ -43,6 +52,7 @@ public class UserServiceImpl implements UserService {
 	@Override
 	//@CachePut缓存新增的或更新的数据到缓存,其中缓存名字是 people 。数据的key是person的id
     @CachePut(key = "#p0.id")
+	@Transactional
 	public User updateUserById(User user) {
 		userMapper.updateByPrimaryKey(user);
 		return user;
@@ -51,11 +61,13 @@ public class UserServiceImpl implements UserService {
 	@Override
 	//@Cacheable缓存key为person 的id 数据到缓存people 中,如果没有指定key则方法参数作为key保存到缓存中。
     @Cacheable(key = "#p0")
+	@Transactional(readOnly=true)
 	public User fingUserById(String id) {
 		return userMapper.selectByPrimaryKey(id);
 	}
 
 	@Override
+	@Transactional(readOnly=true)
 	public Page<User> findUserList(int page, int rows) {
 		PageHelper.startPage(page, rows);
 		return userMapper.findUserList();
